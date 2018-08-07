67a75f54c28a9bb8bcb259dc8dc140faae30698f
Hayley Howell <hayley_howell@sbcglobal.net>
2018-08-07 14:45:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,4 @@ let newID = oldID + 1000000000;
 let ageIsValid = Number.isInteger(currentAge);
 let randomNumber = Math.random(20);
 let randomInteger = Number.parseInt(randomNumber);
+let randomUserId = randomInteger + 1000000000;
